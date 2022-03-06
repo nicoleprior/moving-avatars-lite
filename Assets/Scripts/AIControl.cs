fbb97ffcc5a64e346a300d6ade1c13eb27b7feb3
nicoleprior <53948027+nicoleprior@users.noreply.github.com>
2022-03-06 21:55:42 +0000

    1

---
+++ Assets/Scripts/AIControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour
+{
+
+	GameObject[] goalLocations;
+	UnityEngine.AI.NavMeshAgent agent;
+	Animator anim;
+	private bool goalReached;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		goalLocations = GameObject.FindGameObjectsWithTag("goal");
+		agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+		agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+		anim = this.GetComponent<Animator>();
+		anim.SetFloat("wOffset", Random.Range(0, 1));
+		anim.SetTrigger("isWalking");
+		float sm = Random.Range(0.2f, 0.6f);
+		anim.SetFloat("speedMult", sm);
+		agent.speed *= sm;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (agent.remainingDistance < 1)
+		{
+			if (goalReached == false) { StartCoroutine(stayIdle()); }
+		}
+	}
+
+	IEnumerator stayIdle()
+	{
+		goalReached = true;
+		anim.SetTrigger("isIdle");
+		yield return new WaitForSeconds(Random.Range(1, 3));
+		goalReached = false;
+		anim.SetTrigger("isWalking");
+		agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+	}
+}
